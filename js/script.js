c53bf76a8f0ef5f3209f796037f510b62c6f81f1
Udit Chauhan <161213197+Mr-Udit@users.noreply.github.com>
2024-06-17 10:03:13 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+function showSearch() {
+    var open =  document.getElementById('open');
+    var search = document.getElementById('search');
+    var close = document.getElementById("close");
+    open.style.display = 'none';
+    search.style.display = "block";
+    close.style.display = "block";
+}
+function closeSearch() {
+    var open =  document.getElementById('open');
+    var search = document.getElementById('search');
+    var close = document.getElementById("close");
+    open.style.display = 'block';
+    search.style.display = "none";
+    close.style.display = "none";
+}
+function dark() {
+    var nav = document.getElementById('nav-stylesheet');
+    var status = nav.getAttribute('href');
+    if (status == '/css/nav.css') {
+        nav.setAttribute('href', '/css/nav-dark.css');
+    }
+    else{
+        nav.setAttribute('href', '/css/nav.css');
+    }
+}
+function showMenu() {
+    var x = document.getElementById("menu");
+    var x2 = document.getElementById("openMenu");
+    var x3 = document.getElementById("closeMenu");
+    x.style.display = "block";
+    x2.style.display = "none";
+    x3.style.display = "block";
+}
+function closeMenu() {
+    var x = document.getElementById("menu");
+    var x2 = document.getElementById("openMenu");
+    var x3 = document.getElementById("closeMenu");
+    x.style.display = "none";
+    x2.style.display = "block";
+    x3.style.display = "none";
+}
